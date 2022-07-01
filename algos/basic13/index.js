d2ff03582c64f1cc0c4f6a010dfca01a7b52d484
tmax818 <tylermaxwell661@gmail.com>
2022-07-01 15:44:16 -0700

    init commit

---
+++ algos/basic13/index.js
@@ -0,0 +1,46 @@
+// Print all the integers from 1 to 255.
+function print1to255(){}
+
+// Print integers from 0 to 255, and with each integer print the sum so far.
+function printIntsAndSum0To255(){}
+
+// Given an array, find and print its largest element.
+function printMaxOfArray(arr){}
+
+// Create an array with all the odd integers between 1 and 255 (inclusive).
+function returnOddsArray1to255(){}
+
+// Given an array and a value Y, count and print the number of array values greater than Y.
+function returnArrayCountGreaterThanY(arr, y){}
+
+// Given an array, print the max, min and average values for that array.
+function printMaxMinAverageArrayVals(arr){}
+
+
+// Replace any negative array values with 'Dojo'.
+function swapStringForArrayNegativeVals(arr){}
+
+// Print all odd integers from 1 to 255.
+function printOdds1to255(){}
+
+// Iterate through a given array, printing each value.
+function printArrayVals(arr){}
+
+// Analyze an array’s values and print the average
+function printAverageOfArray(arr){}
+
+// Square each value in a given array, returning that same array with changed values
+function squareArrayVals(arr){}
+
+// Return the given array, after setting any negative values to zero.
+function zeroOutArrayNegativeVals(arr){}
+
+// Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+function shiftArrayValsLeft(arr){}
+
+
+
+
+// DON'T CHANGE CODE BELOW THIS LINE
+
+module.exports = {print1to255, printOdds1to255, printIntsAndSum0To255, printArrayVals, printMaxOfArray, printAverageOfArray, returnArrayCountGreaterThanY, returnOddsArray1to255, squareArrayVals, zeroOutArrayNegativeVals, printMaxMinAverageArrayVals, shiftArrayValsLeft, swapStringForArrayNegativeVals}
