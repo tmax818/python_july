ed510cf2a5d5a6f8eb4c76c73898ec312187e467
tmax818 <tylermaxwell661@gmail.com>
2022-07-05 12:58:19 -0700

    add 13 in py

---
+++ algos/basic13/01day/main.py
@@ -0,0 +1,55 @@
+
+## TODO: Print all the integers from 1 to 255.
+def print1to255():
+    pass
+
+## TODO: Print integers from 0 to 255, and with each integer print the sum so far.
+def printIntsAndSum0To255():
+    pass
+
+## TODO: Given an array, find and print its largest element.
+def printMaxOfArray(arr):
+    pass
+
+## TODO: Create an array with all the odd integers between 1 and 255 (inclusive).
+def returnOddsArray1to255():
+    pass
+
+## TODO: Given an array and a value Y, count and print the number of array values greater than Y.
+def returnArrayCountGreaterThanY(arr, y):
+    pass
+
+## TODO: Given an array, print the max, min and average values for that array.
+def printMaxMinAverageArrayVals(arr):
+    pass
+
+
+## TODO: Replace any negative array values with 'Dojo'.
+def swapStringForArrayNegativeVals(arr):
+    pass
+
+## TODO: Print all odd integers from 1 to 255.
+def printOdds1to255():
+    pass
+
+## TODO: Iterate through a given array, printing each value.
+def printArrayVals(arr):
+    pass
+
+## TODO: Analyze an array’s values and print the average
+def printAverageOfArray(arr):
+    pass
+
+## TODO: Square each value in a given array, returning that same array with changed values
+def squareArrayVals(arr):
+    pass
+
+## TODO: Return the given array, after setting any negative values to zero.
+def zeroOutArrayNegativeVals(arr):
+    pass
+
+## TODO: Given an array, move all values forward by one index, dropping the first and leaving a '0' value at the end.
+def shiftArrayValsLeft(arr):
+    pass
+
+
