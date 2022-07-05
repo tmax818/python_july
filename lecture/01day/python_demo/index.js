d4ad0698268e9c3f569722910312a620ed90aef8
tmax818 <tylermaxwell661@gmail.com>
2022-07-05 12:54:15 -0700

    update

---
+++ lecture/01day/python_demo/index.js
@@ -0,0 +1,9 @@
+
+let sum = 0
+for(let i = 0; i < 500001; i++){
+    if(i % 2 === 1){
+        sum = sum +  i
+    }
+}
+console.log(sum)
+
---
+++ lecture/01day/python_demo/loops.py
@@ -0,0 +1,11 @@
+
+
+start = 5
+stop = 1001
+
+
+for i in range(start, stop):
+    if i % 5 == 0:
+        print(i) 
+
+    
