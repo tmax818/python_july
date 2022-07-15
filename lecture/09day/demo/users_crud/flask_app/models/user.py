794ebbd37c2a6c395894ba663b3a2bd4f302a6b6
tmax818 <tylermaxwell661@gmail.com>
2022-07-15 08:53:34 -0700

    update

--- lecture/09day/demo/users_crud/flask_app/models/user.py
+++ lecture/09day/demo/users_crud/flask_app/models/user.py
@@ -1,6 +1,5 @@
-# import the function that will return an instance of a connection
+
 from flask_app.config.mysqlconnection import connectToMySQL
-# model the class after the user table from our database
 from pprint import pprint
 
 DATABASE = 'users'
@@ -13,16 +12,12 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL(DATABASE).query_db(query)
-        # pprint(results)
-        # Create an empty list to append our instances of users
         users = []
-        # Iterate over the db results and create instances of users with cls.
         for user in results:
             users.append( User(user) )
         return users
@@ -31,7 +26,6 @@ class User:
     def get_one(cls,data):
         query = "SELECT * FROM users WHERE id = %(id)s;"
         result = connectToMySQL(DATABASE).query_db(query, data)
-        print(result)
         user = result[0]
         return User(user)
         
