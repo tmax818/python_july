8059f4722b57727a827b5d30f1c77c2fa9677e3c
tmax818 <tylermaxwell661@gmail.com>
2022-07-15 16:21:39 -0700

    update

--- lecture/10day/demo/login_and_reg/flask_app/controllers/users.py
+++ lecture/10day/demo/login_and_reg/flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask_app import app, render_template, redirect, request
+from flask_app import app, render_template, redirect, request, bcrypt, session
 from flask_app.models.user import User
 
 @app.route('/')
@@ -12,8 +12,38 @@ def register_user():
     if not User.validate_user(request.form):
         return redirect('/')
     # hash password
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
     # create data dict to change password value to the hash created above.
+    data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password":pw_hash
+    }
     # save user to database
+    user_id = User.save(data)
+    # log user in
+    session['user_id'] = user_id
+    session['first_name'] = request.form['first_name']
+    # redirect to app page
+    return redirect('/dashboard')
+
+@app.route('/users/login', methods = ['post'])
+def login_user():
+    # query database for user email from form
+    # use query to redirect if not found
+    # check password in form against password hash in database
+    # redirect if not found
     # log user in
     # redirect to app page
     return redirect('/dashboard')
+
+@app.route('/users/show/<int:id>')
+def show_user(id):
+    # create user show page if necessary
+    return render_template("show_user.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
--- lecture/10day/demo/login_and_reg/flask_app/models/user.py
+++ lecture/10day/demo/login_and_reg/flask_app/models/user.py
@@ -1,4 +1,11 @@
+import re	# the regex module
 from flask_app import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+
+DATABASE = 'login_and_reg'
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
 class User:
 
     def __init__(self, data):
@@ -12,17 +19,29 @@ class User:
 
     @classmethod
     def save(cls, data):
-        pass
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+        ## prepared statement varibles must match keys in data dict
+        return connectToMySQL(DATABASE).query_db(query, data)
 
     @staticmethod
     def validate_user(user:dict):
         is_valid = True
         if len(user['first_name']) < 2:
-            flash("you messed up, try again!!!")
+            flash("you messed up, try again!!!", "first_name")
             is_valid = False
         if len(user['last_name']) < 2:
-            flash("you messed up again, dummy, try again!!!")
+            flash("you messed up again, dummy, try again!!!", "last_name")
             is_valid = False
+        if len(user['password']) < 8:
+            flash("Password must be at least eight characters.", "password")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", "email")
+            is_valid = False
+        if user['password'] != user['password_confirmation']: 
+            flash("passwords must match", "password_confirmation")
+            is_valid = False
+
         
         return is_valid
 
