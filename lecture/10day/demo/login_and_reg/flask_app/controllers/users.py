a54346faaf4a44a35c925c6aaf4b68c01d4cd8d0
tmax818 <tylermaxwell661@gmail.com>
2022-07-15 15:04:07 -0700

    update

--- lecture/10day/demo/login_and_reg/flask_app/controllers/users.py
+++ lecture/10day/demo/login_and_reg/flask_app/controllers/users.py
@@ -8,6 +8,12 @@ def index():
 @app.route('/users/register', methods = ['post'])
 def register_user():
     print(request.form)
+    # validate user info from form
     if not User.validate_user(request.form):
         return redirect('/')
+    # hash password
+    # create data dict to change password value to the hash created above.
+    # save user to database
+    # log user in
+    # redirect to app page
     return redirect('/dashboard')
